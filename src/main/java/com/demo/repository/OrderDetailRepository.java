6797d54f106b4fed1da7f87df173b795ca5ad13c
ccyefan <1169402001@qq.com>
2018-04-25 23:45:58 +0800

    easyui restful

--- src/main/java/com/demo/repository/OrderDetailRepository.java
+++ src/main/java/com/demo/repository/OrderDetailRepository.java
@@ -3,9 +3,10 @@ package com.demo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.demo.dataobject.OrderDetail;
-
+@RepositoryRestResource(collectionResourceRel="order",path="order")
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, String> {
 		/**
 		 * 通过关联外键字段(订单用户表)字段查询订单详情
